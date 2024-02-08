d9e7f44a979eb6c81c6569df92fd2fada762dc57
afajardito <alejandro.afb@hotmail.com>
2024-02-07 19:30:29 -0800

    Initial Commit

---
+++ iOSJourney/iOSJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSJourneyApp.swift
+//  iOSJourney
+//
+//  Created by alejandro fajardo on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct iOSJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
